643aad6311962b03911ed83df0aecbd6cd31cd7b
Nishant Banger <nishantbanger18@gmail.com>
2025-08-06 12:14:34 +0530

    useRef for focus in input

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,21 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const value = useRef(null);
 
+  const handleRef = () =>{
+    value.current.focus();
+  }
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+   <>
+   <div>Nishant Banger <span>{count}</span></div>
+    <input type="text" placeholder='Search item' ref={value} />
+   <button onClick={handleRef} >Increase</button>
+   </>
   )
 }
 
